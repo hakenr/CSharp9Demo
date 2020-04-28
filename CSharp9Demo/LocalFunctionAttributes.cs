807c1d29a96828f66a776aa97aebbe2cdc3b4df2
Robert Haken <haken@havit.cz>
2020-04-28 17:05:14 +0200

    Local Function Attributes

---
+++ CSharp9Demo/LocalFunctionAttributes.cs
@@ -0,0 +1,31 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp9Demo
+{
+	public class LocalFunctionAttributes
+	{
+		public void Demo(string[] args)
+		{
+			if (TryParse("gag", out var x))
+			{
+				Console.WriteLine(x.Length);
+			}
+
+			//  C# 8.0: CS8205 Attributes are not allowed on local function parameters or type parameters
+			static bool TryParse(string input, [NotNullWhen(true)] out string? result)
+			{
+				result = null;
+				return false;
+			}
+		}
+	}
+}
+
+// https://github.com/dotnet/csharplang/issues/1888
+// already in <LangVersion>preview</LangVersion>
