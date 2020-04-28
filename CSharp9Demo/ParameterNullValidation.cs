667ef07379cf110811ce57c443c88bc779942a8f
Robert Haken <haken@havit.cz>
2020-04-28 16:36:03 +0200

    Parmeter null validation

---
+++ CSharp9Demo/ParameterNullValidation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp9Demo
+{
+    public class ParameterNullValidation
+    {
+        public void DoSomething8(string text)
+		{
+			if (text is null)
+			{
+				throw new ArgumentNullException(nameof(text));
+			}
+		}
+
+		public void DoSomething9(string text!) // syntax still not decided
+		{
+			// done
+		}
+    }
+}
+
+// https://github.com/dotnet/csharplang/issues/2145
+// https://github.com/dotnet/csharplang/blob/master/meetings/2019/LDM-2019-07-10.md#param
